1e3a05e246121f03528baf1c9a2d2b6fcf0dbf28
Ramon Felipe <ramonfelipe2007@hotmail.com>
2025-09-04 09:58:03 -0300

    adicionado escolha de quantidade de caracters

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,56 @@
 import Button from '@mui/material/Button'
 import style from './style.module.css'
 import { useState } from 'react'
-import {gerarLetras} from './functions/GeradorLetras'
+
 
 export function LayoutGerador() {
 
     const [senha, setSenha] = useState("")
     const [Copiar, setCopiar] = useState("Copiar")
-    let Copiado =  "Copiado!!!"
+    const [senhaqtd, setQuantidade] = useState()
+
+    
+    const copiarSenha = () => {
+        navigator.clipboard.writeText(senha)
+        setCopiar('Copiado!')
+    }
+
     const gerarSenha = () => {
         setSenha(gerarLetras())  
         setCopiar("Copiar")  
     }
 
+    const gerarLetras = () => {
+    const caracters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    let resultado = "";
+
+    if(senhaqtd <= 0){
+        window.alert('Quantidade Invalida!')
+        return
+    }
+
+    
+    for (let i = 0; i < senhaqtd; i++) {
+        const indice = Math.floor(Math.random() * caracters.length);
+        resultado += caracters[indice];
+    }
+    
+    return resultado
+}
+
     return(
-        <div className='divContainerGerador'>
+        <div className={style.divContainerGerador}>
+
+            <div className={style.divQtd}>
+                <label htmlFor="SenhaQtd">Tamanho:</label>
+                <input 
+                    type="number" 
+                    id="SenhaQtd" 
+                    value={senhaqtd}
+                    onChange={(e) => setQuantidade(e.target.value) }
+                />
+            </div>
+
             <div className={style.divOp}>
                 <Button 
                 variant="contained" 
@@ -25,11 +61,11 @@ export function LayoutGerador() {
                 <Button 
                     variant="contained" 
                     className={style.BtnCopiar} 
-                    onClick={() => setCopiar(Copiado)}
+                    onClick={() => copiarSenha()}
                 >{Copiar}</Button>
             </div>
             <div className={style.divTextResult}>
-                <p>{senha}</p>
+                <p>Senha: {senha}</p>
             </div>
         </div>
     )
